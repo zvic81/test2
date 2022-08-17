68d9b9c831f0eaa54838c06cbbe23bd4e93d25ea
zvic <zvic@list.ru>
2022-08-17 15:46:53 +0300

    changes frov VR zahkarov

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 mstr='Мерцай, мерцай, маленькая звезда, Как я удивляюсь, кто ты! Вверх над миром так высоко, Как алмаз в небе. Мерцай, мерцай, маленькая звезда, Как я удивляюсь, кто ты'
 print(mstr.format(end=','))
 
+print ('me made changes to project from vostochnyi rudnik')
+
 print (mstr)
