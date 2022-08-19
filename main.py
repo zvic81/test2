912e87d7ac15854e8e604117d154e68f89c96c98
zvic81 <zvic@list.ru>
2022-08-19 06:25:23 +0300

    friday beer

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ print ('me made changes to project from vostochnyi rudnik')
 #this is changes from apatity? 18 august 6-22
 a=5
 print (a*a)
-
+print ('привет из пятницы, скоро пивас!')
 print (mstr)
